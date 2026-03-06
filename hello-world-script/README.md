b759aca801cbe05d2f193645ffb225f89688cd3e
boar33 <boarborg@gmail.com>
2026-03-05 20:45:50 -0700

    added bash script and readme

---
+++ hello-world-script/README.md
@@ -0,0 +1,8 @@
+# hello_world
+
+simple script to print Hello, World! to your console.
+
+to run it
+```
+bash hello_world.sh
+```
---
+++ hello-world-script/hello_world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#: Title       : hw
+#: Date        : 05 march 2026
+#: Author      : Bat Erdene
+#: Version     : 1.0
+#: Description : prints Hello, World!
+#: Options     : None
+
+printf "%s\n" "Hello, World!"
